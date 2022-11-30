cb487f88ec204070d51b8edc5af3629545028b91
zaccowan <68406370+zaccowan@users.noreply.github.com>
2022-11-30 15:07:57 -0500

    Removed Unused Files

--- src/Application.java
+++ src/Application.java
@@ -12,7 +12,7 @@ public class Application {
 	 */
 	public static void main(String [] args ) throws MaxRunwayException {
 	
-		Airport airport = new Airport(21);
+		Airport airport = new Airport();
 		airport.runSimulation();
 	}
 	
